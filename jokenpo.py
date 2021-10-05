a7f1bdfb0d65c162c95d552ed98e8181d27f0ea9
Edgar Correa <outedg@gmail.com>
2021-10-05 17:36:44 -0400

    primeiro commit

---
+++ jokenpo.py
@@ -0,0 +1,37 @@
+'''
+DESAFIO
+O objetivo é fazer um juiz de Jokenpo
+que dada a jogada dos dois jogadores
+informa o resultado da partida
+
+REGRAS
+ Não importa o nível de conhecimento
+ Objetivo principal é aprender algo, ou compartilhar algo que sabe
+ Concluir o desafio não é o Objetivo
+ Baby Step (o mais simples/básico)
+ Trabalhar orientado a Teste (TDD) [Red/Green/Refactoring]
+ Piloto e co-piloto (piloto coda, co-piloto dá os palpites)
+ Enquanto os teste não estiverem passando, somente o par pode falar
+ Expectador podem falar, somente quando o teste estiverem passando
+    ou quando pedirem ajuda;
+DESAFIO 
+JOKENPO
+
+
+ANDREY
+LEANDRO
+EDGAR
+LUCAS
+PATRIK
+VINICIUS
+'''
+
+#ATRIBUIÇÕES
+# PEDRA: str = "pedra"
+# EMPATE: str = "empate"
+
+# def jokenpo(player: str, player2: str) -> str:
+#     return EMPATE
+
+# def test_inicial_empate() -> None:
+#     assert jokenpo(PEDRA, PEDRA) == EMPATE
